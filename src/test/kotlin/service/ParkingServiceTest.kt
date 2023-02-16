2fbd07477ff6ea108afab4784fcb3046aad94dbb
vishalb <vishalb@sahaj.ai>
2023-02-17 00:08:17 +0530

    add more testcase which parks two vechile

--- src/test/kotlin/service/ParkingServiceTest.kt
+++ src/test/kotlin/service/ParkingServiceTest.kt
@@ -18,5 +18,19 @@ class ParkingServiceTest {
         Assertions.assertEquals(false, parking.isSpotAvailableAtSpot(ticket.getSpotNumber()))
     }
 
+    @Test
+    fun `should park the second car into second parking spot parking`() {
+
+        val parking = Parking()
+
+        val ticket1: Ticket = ParkingService.parkCarAtParking(parking)
+        val ticket2: Ticket = ParkingService.parkCarAtParking(parking)
+
+        Assertions.assertEquals(1, ticket1.getSpotNumber())
+        Assertions.assertEquals(false, parking.isSpotAvailableAtSpot(ticket1.getSpotNumber()))
+
+        Assertions.assertEquals(2, ticket2.getSpotNumber())
+        Assertions.assertEquals(false, parking.isSpotAvailableAtSpot(ticket2.getSpotNumber()))
+    }
 
 }
